c77e4d4289544151277b478e849256f850b75a9b
Brenden Hyde <brendenahyde@gmail.com>
2022-12-02 15:45:30 -0800

    Progress for day02, part 1

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -3,39 +3,22 @@ use std::fs;
 use std::process;
 
 #[derive(Debug)]
-struct RPS<'a> {
+struct RPS {
     // A struct for holding Rock, Paper, Scissors moves
-    opponent_moves: Vec<&'a str>,
-    my_moves: Vec<&'a str>,
+    opponent_moves: Vec<char>,
+    my_moves: Vec<char>,
 }
 
 fn read_strategy_guide(guide: &str) -> RPS {
-    let mut opponent: Vec<&str> = Vec::new();
-    let mut mine: Vec<&str> = Vec::new();
+    let mut opponent: Vec<char> = Vec::new();
+    let mut mine: Vec<char> = Vec::new();
     let spl = guide.trim().split("\n");
     for line in spl {
         let sp = line.trim().split("\n");
-        let opp = &line.chars().nth(0).unwrap();
-        let my = &line.chars().nth(2).unwrap();
-        match opp {
-            'A' => opponent.push("A"),
-            'B' => opponent.push("B"),
-            'C' => opponent.push("C"),
-            _ => {
-                println!("Unknown character {}", opp.to_string());
-                process::exit(1);
-            }
-        }
-
-        match my {
-            'X' => mine.push("X"),
-            'Y' => mine.push("Y"),
-            'Z' => mine.push("Z"),
-            _ => {
-                println!("Unknown character {}", opp.to_string());
-                process::exit(1);
-            }
-        }
+        let opp = line.chars().nth(0).unwrap();
+        let my = line.chars().nth(2).unwrap();
+        opponent.push(opp);
+        mine.push(my);
     }
 
     RPS { opponent_moves: opponent, my_moves: mine }
