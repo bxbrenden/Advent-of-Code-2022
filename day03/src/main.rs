ba7a719ad57f68847fff36ad69135e98b44dc52a
Brenden Hyde <brendenahyde@gmail.com>
2022-12-04 10:39:54 -0800

    solution for Day03, part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -32,7 +32,8 @@ fn find_triples(s: &String) -> Vec<usize> {
     triples
 }
 
-fn find_badges(puzzle_input: &String) {
+fn part2_answer(puzzle_input: &String, indices: Vec<usize>) -> u32 {
+    let mut final_count: u32 = 0;
     let mut priorities: HashMap<char, u32> = HashMap::new();
     let alphabet: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         .chars()
@@ -43,12 +44,8 @@ fn find_badges(puzzle_input: &String) {
         priorities.insert(*letter, index.try_into().unwrap());
     }
 
-    let mut stream: Vec<&str> = Vec::new();
-}
-
-fn part2_answer(puzzle_input: &String, indices: Vec<usize>) -> () {
     let mut chunks: Vec<&str> = Vec::new();
-    for (i, t) in indices.iter().enumerate() {
+    for (i, _) in indices.iter().enumerate() {
         match i {
             0 => {
                 chunks.push(&puzzle_input[..indices[i]]);
@@ -58,10 +55,10 @@ fn part2_answer(puzzle_input: &String, indices: Vec<usize>) -> () {
             }
         }
     }
-    println!("{:?}", chunks);
+    // println!("{:?}", chunks);
 
     for chunk in chunks {
-        println!("Starting chunk: {chunk}");
+        // println!("Starting chunk: {chunk}");
         let mut lines: Vec<&str> = Vec::new();
         for line in chunk.trim().split("\n") {
             lines.push(line);
@@ -77,29 +74,31 @@ fn part2_answer(puzzle_input: &String, indices: Vec<usize>) -> () {
         for c in lines[0].chars() {
             h1.insert(c);
         }
-        println!("h1 contains: {:?}", h1);
+        // println!("h1 contains: {:?}", h1);
 
         for c in lines[1].chars() {
             h2.insert(c);
         }
-        println!("h2 contains: {:?}", h2);
+        // println!("h2 contains: {:?}", h2);
 
         for c in lines[2].chars() {
             h3.insert(c);
         }
-        println!("h3 contains: {:?}", h3);
+        // println!("h3 contains: {:?}", h3);
 
         let h1h2 = h1.intersection(&h2);
         for c in h1h2 {
             h4.insert(*c);
         }
-        println!("h4 contains: {:?}", h4);
+        // println!("h4 contains: {:?}", h4);
 
         let h3h4 = h3.intersection(&h4);
         for entry in h3h4 {
-            println!("{}", entry);
+            // println!("{}", entry);
+            final_count += priorities[entry];
         }
     }
+    final_count
 }
 
 fn find_common_elements(puzzle_input: &String) -> Vec<u32> {
@@ -146,11 +145,12 @@ fn find_common_elements(puzzle_input: &String) -> Vec<u32> {
 
 fn main() {
     let contents = read_puzzle_input();
-    println!("{}", contents);
+    // println!("{}", contents);
 
-    let errors = find_common_elements(&contents);
-    println!("{:?}", errors.iter().sum::<u32>());
+    let _errors = find_common_elements(&contents);
+    // println!("{:?}", errors.iter().sum::<u32>());
     let triples = find_triples(&contents);
-    println!("{:?}", triples);
-    part2_answer(&contents, triples);
+    // println!("{:?}", triples);
+    let answer2 = part2_answer(&contents, triples);
+    println!("{}", answer2);
 }
