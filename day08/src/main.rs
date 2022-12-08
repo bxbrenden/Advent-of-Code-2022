59a36bba8bd0fc74394a9e7fd5769ecabd0bfda2
Brenden Hyde <brendenahyde@gmail.com>
2022-12-08 15:58:13 -0800

    broken code for day 8, part 2, but progress 🤷

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -67,6 +67,75 @@ fn find_visible_trees(tree_grid: &Vec<Vec<u32>>) -> usize {
     visible
 }
 
+fn highest_scenic_score(tree_grid: &Vec<Vec<u32>>) -> u32 {
+    let mut highest: u32 = 0;
+    for (y, row) in tree_grid.iter().enumerate() {
+        for (x, column) in row.iter().enumerate() {
+            let score = get_scenic_score(tree_grid, x, y);
+            highest = if score > highest { score } else { highest };
+        }
+    }
+
+    highest
+}
+
+fn get_scenic_score(tree_grid: &Vec<Vec<u32>>, x_loc: usize, y_loc:usize) -> u32 {
+    let grid_max = get_grid_dimensions(tree_grid);
+    let x_max: usize = grid_max.0 - 1;
+    let y_max: usize = grid_max.1 - 1;
+
+    let mut top_val: u32 = 0;
+    let mut left_val: u32 = 0;
+    let mut right_val: u32 = 0;
+    let mut bottom_val: u32 = 0;
+
+    if x_loc == 0 || y_loc == 0 || x_loc == x_max || y_loc == y_max {
+        return 0;
+    }
+
+    // Check left score by decrementing x values
+    let mut xl = x_loc;
+    let yl = y_loc;
+    while xl > 1 {
+        xl -= 1;
+        let test = tree_grid[yl][xl];
+        left_val = if test > left_val {test} else {left_val};
+    }
+
+    // Check top score by decrementing y values
+    let xt = x_loc;
+    let mut yt = y_loc;
+    while yt > 1 {
+        yt -= 1;
+        let test = tree_grid[yt][xt];
+        top_val = if test > top_val {test} else {top_val};
+    }
+
+    // Check right score by incrementing x values
+    let mut xr = x_loc;
+    let yr = y_loc;
+    while xr < x_max - 1 {
+        xr += 1;
+        let test = tree_grid[yr][xr];
+        right_val = if test > right_val {test} else {right_val};
+    }
+
+    // Check bottom score by incrementing y values
+    let xb = x_loc;
+    let mut yb = y_loc;
+    while yb < y_max - 1 {
+        yb += 1;
+        let test = tree_grid[yb][xb];
+        bottom_val = if test > bottom_val {test} else {bottom_val};
+    }
+
+    println!("({}, {}) {}: (left){} *(right) {} *(top) {} *(bottom) {} = {}",
+             y_loc, x_loc, tree_grid[y_loc][x_loc], left_val, right_val, top_val, bottom_val,
+             (left_val * right_val * top_val * bottom_val)
+    );
+    left_val * right_val * top_val * bottom_val
+}
+
 fn is_visible(tree_grid: &Vec<Vec<u32>>, x_loc: usize, y_loc: usize) -> Vec<bool> {
     if y_loc == 0 || x_loc == 0 {
         println!("Returning early");
@@ -75,8 +144,8 @@ fn is_visible(tree_grid: &Vec<Vec<u32>>, x_loc: usize, y_loc: usize) -> Vec<bool
     let mut x = x_loc;
     let mut y = y_loc;
     let grid_dims = get_grid_dimensions(tree_grid);
-    let max_x = grid_dims.1 - 1 as usize;
-    let max_y = grid_dims.0 - 1 as usize;
+    let max_x = grid_dims.1 - 1;
+    let max_y = grid_dims.0 - 1;
     println!("Max X: {max_x}, Max Y: {max_y}");
     if y_loc == max_y || x_loc == max_x {
         println!("Returning early");
@@ -149,4 +218,7 @@ fn main() {
 
     let visible = find_visible_trees(&tree_grid);
     println!("{} visible trees", visible);
+
+    let highest = highest_scenic_score(&tree_grid);
+    println!("Highest score: {highest}");
 }
