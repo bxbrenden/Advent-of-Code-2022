81fa2f2d43684bc03dc80ba99d149c9eb7606a98
Brenden Hyde <brendenahyde@gmail.com>
2022-12-07 20:46:48 -0800

    progress on day 7, part 1

--- day07/day7.py
+++ day07/day7.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import sys
 
 
@@ -12,16 +13,44 @@ def read_puzzle_input():
 
     try:
         with open(file_path, "r") as f:
-            puz = f.read().strip()
+            puz = f.read().strip().split("\n")
             return puz
     except (FileNotFoundError, PermissionError) as e:
         raise SystemExit(f"Failed to open file '{file_path}':\n{e}")
 
 
+def process_commands(lines, fs_bytes, cwd):
+    """Process the Unix commands."""
+    line = lines[0]
+    if line.startswith("$ cd"):
+        if line == '$ cd /':
+            cwd = Path("/")
+        elif line == '$ cd ..':
+            cwd = cwd.parent.absolute()
+        else:
+            cwd /= line[5:]
+        print(f"Current directory is now: {cwd}")
+    elif line.startswith("$ ls"):
+        print("found an `ls` command.")
+
+    if len(lines) > 1:
+        return process_commands(lines[1:], fs_bytes, cwd)
+    else:
+        return fs_bytes
+
+
+
+def is_command(line):
+    """Return True if the line is a Unix command, else False."""
+    return line.startswith("$ ")
+
+
 def main():
     puz = read_puzzle_input()
-    print(puz)
+    cwd = Path("/")
+    fs_bytes = 0
 
+    fs_bytes = process_commands(puz, fs_bytes, cwd)
 
 if __name__ == "__main__":
     main()
