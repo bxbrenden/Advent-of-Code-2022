2ac3df3869acc5349427f14bcc01d9a04d3627d6
Brenden Hyde <brendenahyde@gmail.com>
2022-12-07 22:43:56 -0800

    progress for day 7, part 1

--- day07/day7.py
+++ day07/day7.py
@@ -3,12 +3,11 @@ import sys
 
 
 class Dir:
-    """All of Dir's children are also of type Dir."""
     def __init__(self, path, files=None, children=None):
         if children is not None:
             self.children = children
         else:
-            self.children = []
+            self.children = dict()
         if files is not None:
             self.files = files
         else:
@@ -16,9 +15,20 @@ class Dir:
         self.path = Path(path)
 
     def __repr__(self):
-        child_names = [x.path.name for x in self.children]
+        child_names = [x for x in self.children.keys()]
         file_names = [x.split()[1] for x in self.files]
         return f"Dir<{self.path.absolute()}, children: {child_names}, files: {file_names}>"
+
+    def tree(self):
+        num_slashes = str(self.path.absolute()).count("/")
+        indent = " " * (num_slashes * 2)
+        for file in self.files:
+            spl = file.split()
+            print(f"{indent}- {spl[1]} (file, size={spl[0]})")
+
+        for child in self.children:
+            print(f"{indent}- {child} (dir)")
+            self.children[child].tree()
 
     def files_size(self):
         size = 0
@@ -29,7 +39,7 @@ class Dir:
     def size(self):
         size = 0
         for child in self.children:
-            size += child.size()
+            size += self.children[child].size()
         size += self.files_size()
         return size
 
@@ -51,38 +61,53 @@ def read_puzzle_input():
         raise SystemExit(f"Failed to open file '{file_path}':\n{e}")
 
 
-def process_commands(lines, direc):
+def process_commands(lines, direc, prev_direc=None):
     """Process the Unix commands."""
     line = lines[0]
+    print(f"Current raw line: {line}")
     if line.startswith("$ cd"):
+        print(f"Found a `cd` command: {line}")
+        # a `cd /` command happens only once at the beginning of every file
         if line == '$ cd /':
-            cwd = Path("/")
+            if len(lines) > 1:
+                return process_commands(lines[1:], direc)
         elif line == '$ cd ..':
-            cwd = cwd.parent.absolute()
+            if prev_direc is not None:
+                if len(lines) > 1:
+                    return process_commands(lines[1:], prev_direc, direc)
+            else:
+                raise SystemExit("No previous directory to return to")
         else:
-            cwd /= line[5:]
-        print(f"Current directory is now: {cwd}")
+            new_cwd = line.replace("$ cd ", "")
+            print(f"new_cwd value: {new_cwd}")
+            if len(lines) > 1:
+                return process_commands(lines[1:], direc.children[new_cwd], direc)
+        print(f"Current directory is now: {direc.path.absolute()}")
     elif line.startswith("$ ls"):
-        print("found an `ls` command.")
+        print(f"found an `ls` command: {line}")
         dir_lines = []
-        cont_line = 0
-        for line_num, line in enumerate(lines[1:]):
-            if not line.startswith("$"):
-                dir_lines.append(line)
+        cont_line = 1
+        for line_num, l in enumerate(lines[1:]):
+            if not l.startswith("$"):
+                print(f"Appending {l} to dir_lines")
+                dir_lines.append(l)
             else:
                 cont_line = line_num
+                break
         for dl in dir_lines:
-            if not dl.startswith("dir"):
-                print(f"file {filename}: {byte_val} bytes")
-
+            if dl.startswith("dir"):
+                new_dirname = dl.split()[1]
+                d = Dir(direc.path.absolute() / new_dirname)
+                print(f"Creating new child dir: {d}")
+                print(f"The dict key for the new child dir is: {new_dirname}")
+                direc.children[new_dirname] = d
             else:
-                #Create Dir objects.
-                pass
-    if len(lines) > 1:
-        return process_commands(lines[1:], fs_bytes, cwd)
-    else:
-        return fs_bytes
-
+                print(f"Found a file: {dl}")
+                print(f"Appending file to directory \"{direc.path.absolute()}\": {dl}")
+                direc.files.append(dl)
+        if len(lines) > 1:
+            print(f"cont_line value: {cont_line}")
+            return process_commands(lines[cont_line:], direc)
 
 
 def is_command(line):
@@ -92,10 +117,13 @@ def is_command(line):
 
 def main():
     puz = read_puzzle_input()
+    print("Creating root directory with path '/'")
     root_dir = Dir("/")
 
     process_commands(puz, root_dir)
-    print(f"Total bytes: {fs_bytes}")
+    print(f"Total size: {root_dir.size()}")
+    root_dir.tree()
+
 
 if __name__ == "__main__":
     main()
