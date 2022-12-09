006b2b46331942bd60ed9cd69c9aa18961fac7d6
Brenden Hyde <brendenahyde@gmail.com>
2022-12-08 22:12:44 -0800

    add basic data structures for rope

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,6 +1,34 @@
 use std::env;
 use std::fs;
 
+#[derive(Debug)]
+struct Pos {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug)]
+struct Rope {
+    head: Pos,
+    tail: Pos,
+    visited: Vec<(i32, i32)>,
+}
+
+impl Pos {
+    fn new(x: i32, y:i32) -> Self {
+        Pos { x: x, y: y }
+    }
+}
+
+impl Rope {
+    fn new() -> Self {
+        let head = Pos::new(0, 0);
+        let tail = Pos::new(0, 0);
+        let visited = vec!((0, 0));
+        Rope { head: head, tail: tail, visited: visited }
+    }
+}
+
 fn read_puzzle_input() -> String {
     let args: Vec<String> = env::args().collect();
     let file_path = match args.len() {
@@ -17,4 +45,7 @@ fn read_puzzle_input() -> String {
 fn main() {
     let puz: String = read_puzzle_input();
     println!("{}", puz);
+
+    let rope = Rope::new();
+    println!("{:?}", rope);
 }
