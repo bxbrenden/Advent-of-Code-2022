cace09906803940cd65ce714c112954a98abbeb7
Brenden Hyde <brendenahyde@gmail.com>
2022-12-12 22:06:37 -0800

    create basic data structure for Monkey day 11, part 1

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,6 +1,25 @@
 use std::env;
 use std::fs;
 
+#[derive(Debug)]
+struct Monkey {
+    start: Vec<i32>,
+    oper: String,
+    test: i32,
+    t: usize,
+    f: usize,
+}
+
+impl Monkey {
+    fn new(start: Vec<i32>,
+           oper: String,
+           test: i32,
+           t: usize,
+           f: usize) -> Self {
+        Monkey { start, oper, test, t, f}
+    }
+}
+
 fn read_puzzle_input() -> String {
     let args: Vec<String> = env::args().collect();
     let file_path = match args.len() {
@@ -13,7 +32,16 @@ fn read_puzzle_input() -> String {
         .expect("Failed to read puzzle input: {file_path}");
     puz
 }
+
 fn main() {
     let puz = read_puzzle_input();
     println!("{}", puz);
+    let m1 = Monkey::new(
+        vec!(23, 31, 6),
+        "old * 3".to_string(),
+        7,
+        6usize,
+        3usize
+    );
+    println!("{:?}", m1);
 }
