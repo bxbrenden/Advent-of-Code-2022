f69ee69acdaaa87975dd2d95d121983bde4e51fb
Brenden Hyde <brendenahyde@gmail.com>
2022-12-12 22:58:30 -0800

    I suck at closures. Making 24 monkeys instead of 4? Day 11, part 1

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 
 #[derive(Debug)]
 struct Monkey {
+    name: String,
     start: Vec<i32>,
     oper: String,
     test: i32,
@@ -11,12 +12,15 @@ struct Monkey {
 }
 
 impl Monkey {
-    fn new(start: Vec<i32>,
-           oper: String,
-           test: i32,
-           t: usize,
-           f: usize) -> Self {
-        Monkey { start, oper, test, t, f}
+    fn new(name: String, start: Vec<i32>, oper: String, test: i32, t: usize, f: usize) -> Self {
+        Monkey {
+            name,
+            start,
+            oper,
+            test,
+            t,
+            f,
+        }
     }
 }
 
@@ -28,20 +32,66 @@ fn read_puzzle_input() -> String {
         _ => "sample_input.txt",
     };
 
-    let puz = fs::read_to_string(file_path)
-        .expect("Failed to read puzzle input: {file_path}");
+    let puz = fs::read_to_string(file_path).expect("Failed to read puzzle input: {file_path}");
     puz
+}
+
+fn parse_input(puz: &String, monkeys: &mut Vec<Monkey>) {
+    let blocks = puz.trim().split("\n\n");
+    for block in blocks {
+        let lines = block.split("\n");
+        for (index, line) in lines.enumerate() {
+            let mut name: String = String::new();
+            let mut start: Vec<i32> = Vec::new();
+            let mut oper: String = String::new();
+            let mut test: i32 = 0;
+            let mut t: usize = 0;
+            let mut f: usize = 0;
+            match index {
+                0 => name = line.to_string(),
+                1 => {
+                    line.replace("  Starting items: ", "")
+                        .split(", ")
+                        .map(|s| s.parse::<i32>().unwrap())
+                        .map(|n| start.push(n));
+                }
+                2 => oper = line.replace("  Operation: ", "").to_string(),
+                3 => {
+                    test = line.replace("  Test: divisible by ", "")
+                        .parse::<i32>()
+                        .unwrap();
+                }
+                4 => {
+                    t = line.replace("    If true: throw to monkey ", "")
+                        .parse::<usize>()
+                        .unwrap();
+                }
+                5 => {
+                    f = line.replace("    If false: throw to monkey ", "")
+                        .parse::<usize>()
+                        .unwrap();
+                }
+                _ => panic!("Unexpected index in block!"),
+            }
+            let m = Monkey::new(name.clone(), start.clone(), oper.clone(), test, t, f);
+            monkeys.push(m)
+        }
+    }
 }
 
 fn main() {
     let puz = read_puzzle_input();
-    println!("{}", puz);
-    let m1 = Monkey::new(
-        vec!(23, 31, 6),
-        "old * 3".to_string(),
-        7,
-        6usize,
-        3usize
-    );
-    println!("{:?}", m1);
+    let mut monkeys: Vec<Monkey> = Vec::new();
+    parse_input(&puz, &mut monkeys);
+    println!("{:#?}", monkeys);
+
+    // Let m1 = Monkey::new(
+    //     "Monkey 0".to_string(),
+    //     vec![23, 31, 6],
+    //     "old * 3".to_string(),
+    //     7,
+    //     6usize,
+    //     3usize,
+    // );
+    // Println!("{:?}", m1);
 }
